09dcced53328ef636dc32b85196da3cfb0f9e1e8
alby0203 <aj333@cet.ac.in>
2022-04-11 14:31:54 +0530

    sec

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Footer from './footer/Footer';
 import Header from './Header/Header';
 
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
+        
     );
 }
 
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+const Footer = () =>{
+    return(
+        <Container>
+            <Row>
+                <Column>
+                About Us
+                </Column>
+            </Row>
+        </Container>
+
+
+    );
+}
+
+export default Footer;
