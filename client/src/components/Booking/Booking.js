1d3a3d65d7853f4621a6e7c823a71166fab44e13
ireneelizabethsabu <irenepalanatt@gmail.com>
2022-04-11 17:52:31 +0530

    adding calender - disabled booked dates

--- client/src/components/Booking/Booking.js
+++ client/src/components/Booking/Booking.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
-
+import React, { useState } from "react";
+import { Container, Col, Button,Row } from "react-bootstrap";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import './Booking.css'
 
 const Booking = () => {
-const booked = ['Tue Apr 19 2022 00:00:00 GMT+0530 (India Standard Time)'];
-const [value, setValue] = useState(new Date());
+  const booked = ["2022-04-04", "2022-04-05", "2022-04-06", "2022-04-07"];
 
-const dayClick = (e) => {
-    //setValue(e);
-    //console.log(e);
-}
-const disableDay = (date) => {
-    console.log(new Date(date).toString())
-    const found = booked.includes(new Date(date).toString())
-    //console.log(found)
-    return found;
-}
+  const [value, setValue] = useState(new Date());
+
+  const disableDay = ({ activeStartDate, date, view }) => {
+    const found = booked.find((ele) => {
+      const eledate = new Date(ele);
+      return (
+        date.getDate() === eledate.getDate() &&
+        date.getMonth() === eledate.getMonth() &&
+        date.getFullYear() === eledate.getFullYear()
+      );
+    });
+    if (found) return true;
+    return false;
+  };
 
   return (
-    <div>
-      <Calendar  onChange={setValue} value={value} tileDisabled={() => disableDay(value)}/>
-    </div>
+    <Container className="my-5 d-flex justify-content-center">
+      <Col>
+        <Calendar onChange={setValue} value={value} tileDisabled={disableDay} />
+      </Col>
+      <Col>
+      {new Date(value).toDateString()}
+      <Col className="m-3">
+          <Button>Book Slot</Button>
+      </Col>
+      
+      </Col>
+    </Container>
   );
-}
+};
 
 export default Booking;
